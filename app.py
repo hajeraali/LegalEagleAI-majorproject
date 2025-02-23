dd721f75919c1ed2b2656b4001eee8c39336eda1
RianAlp <140075485+RianAlp@users.noreply.github.com>
2025-02-23 18:30:40 +0530

    appointment alert updated

--- app.py
+++ app.py
@@ -256,7 +256,6 @@ def connect_db():
     )
 
 # Function to create the appointments table if it doesn’t exist
-# Function to create the appointments table if it doesn’t exist
 def create_table():
     conn = connect_db()
     cur = conn.cursor()
